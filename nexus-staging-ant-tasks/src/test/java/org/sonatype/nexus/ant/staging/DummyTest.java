13906400735cb99baa2f1904ef197d373f28eee5
Alin Dreghiciu <adreghiciu@gmail.com>
2012-11-28 23:21:58 +0200

    Add a dummy test & fix poms
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

---
+++ nexus-staging-ant-tasks/src/test/java/org/sonatype/nexus/ant/staging/DummyTest.java
@@ -0,0 +1,17 @@
+package org.sonatype.nexus.ant.staging;
+
+import org.junit.Test;
+
+/**
+ * @since 1.2
+ */
+public class DummyTest
+{
+
+    @Test
+    public void dummy()
+    {
+        // FIXMe: This is here to allow build automation configuration to be uniform, really should put some real tests
+    }
+
+}
